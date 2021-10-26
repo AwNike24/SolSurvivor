1f48703dfde88410e2be1918ff0eab95d0051b63
c-jleal <cristian@hostienda.com>
2021-10-26 17:24:48 -0400

    estilos

--- app/src/components/layouts/ApplicationLayout.vue
+++ app/src/components/layouts/ApplicationLayout.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <header-component />
+    <header-dashboar-component v-if="dashboar" />
+    <header-component v-if="!dashboar" />
     <router-view />
     <footer-component />
   </div>
@@ -8,13 +9,25 @@
 
 <script>
 import HeaderComponent from "@/components/header/HeaderComponent.vue";
+import HeaderDashboarComponent from "@/components/header/HeaderDashboarComponent.vue";
 import FooterComponent from "@/components/footer/FooterComponent.vue";
 
 export default {
   name: "ApplicationLayout",
   components: {
     FooterComponent,
+    HeaderDashboarComponent,
     HeaderComponent,
+  },
+  data() {
+    return {
+      dashboar : false,
+    };
+  },
+  created() {
+    if (this.$route.path !== '/') {
+      this.dashboar = true;
+    }
   },
 };
 </script>
